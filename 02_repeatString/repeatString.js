0a8c12f0fdf03c772234ae6c9bcaf0f81dff8121
jkuchtey <113455067+jkuchtey@users.noreply.github.com>
2024-07-15 20:45:17 +0800

    finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, count) {
+    if(count < 0){
+        return "ERROR";
+    }
+    else if(count == 0 || string == ""){
+        return "";
+    }
+    else if(count == 1){
+        return string;
+    }
+    else {
+        let ans = string;
+        for(let i = 0; i < count-1; i++){
+            ans += string;
+        }
+        return ans;
 
+    }
 };
 
 // Do not edit below this line
