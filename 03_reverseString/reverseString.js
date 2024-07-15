d3d702528fd2d56da3f11e6d242cbbb805ac8f9f
jkuchtey <113455067+jkuchtey@users.noreply.github.com>
2024-07-15 20:54:03 +0800

    finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,19 @@
-const reverseString = function() {
-
+const reverseString = function(st) {
+    
+    if(typeof(st) != "string"){
+        return "ERROR: Wrong input type"
+    }
+    else if(st == ""){
+        return "";
+    }
+    else {
+        stArr = st.split("")
+        finalSt = ""
+        for(let i = stArr.length-1; i >= 0; i--){
+            finalSt += stArr[i];
+        }
+        return finalSt;
+    }
 };
 
 // Do not edit below this line
