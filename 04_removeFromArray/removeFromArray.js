d294021037ed2bc389ac9b9fcf37303bab7ebd6f
jkuchtey <113455067+jkuchtey@users.noreply.github.com>
2024-07-25 12:36:14 +0800

    finihsed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...targets) {
+    //complicated solution: loop thru every target, and splice out the idx if it is equal to a target.
+    // for(i = 0; i < targets.length; i++){
+    //     if(arr.includes(targets[i])){
+    //         for(j = 0; j < arr.length; j++){
+    //             if(arr[j] === targets[i]){
+    //                 arr.splice(j, 1);
+    //                 j--;
+    //             }
+    //         }
+    //     }
+
+    // }
+    // return arr;
+
+    //Less complicated: use filter to only include elements which are not included in the targets array.
+    return arr.filter(val => (!targets.includes(val)))
 };
 
 // Do not edit below this line
