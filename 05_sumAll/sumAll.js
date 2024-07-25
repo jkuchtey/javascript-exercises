6e6183ee160468cb4ea2711b6e727d83da5ea0df
jkuchtey <113455067+jkuchtey@users.noreply.github.com>
2024-07-25 12:58:25 +0800

    finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(start, finish) {
+    if(start < 0 || finish < 0 || !(Number.isInteger(start)) || !(Number.isInteger(finish))){return "ERROR"}
+    if (start > finish){[start, finish] = [finish, start]}
+    let res = start;
+    for(i = start+1; i <= finish; i++){res += i}
+    return res;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
