83ad2d33028f12577156b56e731b79373192ab58
jkuchtey <113455067+jkuchtey@users.noreply.github.com>
2024-07-29 23:42:34 +0800

    finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+
+const findTheOldest = function(people) {
+    const lifespans = people.map((person) => {
+        if(person.yearOfDeath === undefined){
+            return 2024 - person.yearOfBirth;
+        }
+        else {
+            return person.yearOfDeath - person.yearOfBirth;
+        }
+    });
+    const oldestIdx = lifespans.findIndex((i) => i === Math.max(...lifespans));
+    return people[oldestIdx];
+
 
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
+
