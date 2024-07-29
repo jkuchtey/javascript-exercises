75e8e4edc419af864c16a4546bbdd57de2f678ef
jkuchtey <113455067+jkuchtey@users.noreply.github.com>
2024-07-29 21:40:54 +0800

    finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(idx) {
+    if(typeof(idx) != "number"){
+        idx = Number(idx);
+    }
+    if(idx < 0){
+        return "OOPS";
+    }
+    if(idx === 0){
+        return 0;
+    }  
+    if(idx === 1){
+        return 1;
+    }
+
+    return fibonacci(idx-1) + fibonacci(idx-2);
+    
 
 };
 
